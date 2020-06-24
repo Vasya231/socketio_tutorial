43c9e52b715d70d0fad39ce264ab4853104c9b1a
Vasya231 <gg231589@gmail.com>
2020-06-24 13:12:52 +0300

    add index.html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import http from 'http';
+import path from 'path';
 
 const app = express();
 const httpServer = http.createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, world!</h1>');
+  const pathToIndex = path.join(__dirname, '../assets/index.html');
+  res.sendFile(pathToIndex);
 });
 
 httpServer.listen(3000, () => console.log('Listening on *:3000'));
